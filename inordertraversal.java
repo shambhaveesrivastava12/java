bad46d889e43fda88143a2a1b82d733ac773fdbf
Shambhavee Srivastava <110680194+ShamsriGithub@users.noreply.github.com>
2023-02-23 17:24:59 +0000

    inorder traversal

---
+++ inordertraversal.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+     List<Integer> arr = new ArrayList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+      
+        if (root!=null){
+        inorderTraversal(root.left);
+        arr.add(root.val);
+        inorderTraversal(root.right);
+            }
+            return arr;
+    }
+}   
